d0a136e9fa1b09a407154b5c64e7485fa277b4b1
artemkotov <art.kotov@gmail1.com>
2020-03-17 13:55:39 +0300

    add side panel

--- js/js.js
+++ js/js.js
@@ -1,20 +1,23 @@
 const sidePanel = document.querySelector('.side-panel__wrapper');
 const sideOpen = document.querySelectorAll('.side-panel__open');
 const sideClose = document.querySelector('.side-panel__close-btn');
-console.log({
-    sidePanel,
-    sideOpen,
-    sideClose
-})
+
+const searchIcon = document.querySelector(".search-icon");
+const search = document.querySelector(".header__search");
 
 sideOpen.forEach(item => {
     item.addEventListener('click', () => {
         console.log('toggle')
         sidePanel.classList.toggle("type--visible")
     })
-})
+});
 
 sideClose.addEventListener('click', () => {
     console.log('toggle')
     sidePanel.classList.toggle("type--visible")
+});
+
+searchIcon.addEventListener("click", () => {
+    console.log('search')
+    search.classList.add("type--visible")
 })
